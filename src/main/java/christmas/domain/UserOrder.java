53aff2a9a75cbb4c91b16ab484a398cd7481a7b2
K-KY <kyuyoungk@naver.com>
2023-12-08 17:27:18 +0900

    Feat(UserOrder) : 사용자의 주문을 형식에 맞춰 저장 할 일급 컬렉션

---
+++ src/main/java/christmas/domain/UserOrder.java
@@ -0,0 +1,41 @@
+package christmas.domain;
+
+import christmas.domain.menu.MenuBoard;
+import christmas.util.validator.NumberValidator;
+import java.util.HashMap;
+import java.util.List;
+
+public class UserOrder {
+    private final HashMap<MenuBoard, Integer> userOrder = new HashMap<>();
+    public UserOrder(List<String> orders) {
+        orders.forEach(this::splitOrder);
+    }
+
+    private void splitOrder(String order) {
+        String[] orderAndAmount = order.split("-");
+        MenuBoard menuBoard = MenuBoard.orderMenu(orderAndAmount[0]);
+        int amount = getAmount(orderAndAmount);
+        userOrder.put(menuBoard, amount);
+    }
+
+    private int getAmount(String[] orderAndAmount) {
+        validOrder(orderAndAmount);
+        NumberValidator.validNumber(orderAndAmount[1]);
+        int amount = Integer.parseInt(orderAndAmount[1]);
+        isInRange(amount);
+        return amount;
+    }
+
+    private void validOrder(String[] orderAndAmount) {
+        if (orderAndAmount.length != 2) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다.");
+        }
+    }
+
+    private void isInRange(int amount) {
+        if (amount > 20 || amount < 1) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다.");
+        }
+    }
+
+}
