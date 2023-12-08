fb7dbae814bb4bfcdd75776250cae00d16c46a87
K-KY <kyuyoungk@naver.com>
2023-12-08 14:33:36 +0900

    Feat(ChristmasBadge) : 혜택 금액에 따라 배지를 리턴하는 enum클래스

---
+++ src/test/java/christmas/ChristmasBadgeTest.java
@@ -0,0 +1,21 @@
+package christmas;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.ChristmasBadge;
+import org.junit.jupiter.api.Test;
+
+public class ChristmasBadgeTest {
+
+    @Test
+    void badgeReturnTest() {
+        assertThat(ChristmasBadge.getBadge(1000)).isEqualTo(ChristmasBadge.NO_BADGE);
+        assertThat(ChristmasBadge.getBadge(4999)).isEqualTo(ChristmasBadge.NO_BADGE);
+        assertThat(ChristmasBadge.getBadge(5000)).isEqualTo(ChristmasBadge.STAR);
+        assertThat(ChristmasBadge.getBadge(9999)).isEqualTo(ChristmasBadge.STAR);
+        assertThat(ChristmasBadge.getBadge(10000)).isEqualTo(ChristmasBadge.TREE);
+        assertThat(ChristmasBadge.getBadge(19999)).isEqualTo(ChristmasBadge.TREE);
+        assertThat(ChristmasBadge.getBadge(20000)).isEqualTo(ChristmasBadge.SANTA);
+        assertThat(ChristmasBadge.getBadge(30000)).isEqualTo(ChristmasBadge.SANTA);
+    }
+}
